8cf5679d3b2f935c9480bddd1ce336aada7257b0
DEM DEMOCRATIC <12345kirill59@gmail.com>
2023-12-12 22:28:33 +0500

    fsf

---
+++ Equipments/ECN_e.cs
@@ -0,0 +1,38 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reader
+{
+    internal class ECN_e
+    {
+        public static ECN CreateExcelEntity(SpreadsheetDocument document, Row row)
+        {
+            string ID = GetCellValue(document, row.Elements<Cell>().ElementAt(0));
+            string N = GetCellValue(document, row.Elements<Cell>().ElementAt(1));
+            string H = GetCellValue(document, row.Elements<Cell>().ElementAt(2));
+            string L = GetCellValue(document, row.Elements<Cell>().ElementAt(3));
+
+            return new ECN(ID, N, H, L);
+        }
+
+        private static string GetCellValue(SpreadsheetDocument document, Cell c)
+        {
+            SharedStringTablePart sharedStringPart = document.WorkbookPart.SharedStringTablePart;
+
+            if (c.DataType != null && c.DataType.Value == CellValues.SharedString)
+            {
+                int index = int.Parse(c.InnerText);
+                return sharedStringPart.SharedStringTable.Elements<SharedStringItem>().ElementAt(index).InnerText;
+            }
+            else
+            {
+                return c.InnerText;
+            }
+        }
+    }
+}
---
+++ Equipments/SHVN_Nasos_e.cs
@@ -0,0 +1,56 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reader
+{
+    internal class SHVN_Nasos_e
+    {
+        public static SHVN_Nasos CreateExcelEntity(SpreadsheetDocument document, Row row)
+        {
+            string Name = GetCellValue(document, GetCellAtIndex(row, 0));
+            string q100 = GetCellValue(document, GetCellAtIndex(row, 1));
+            string H = GetCellValue(document, GetCellAtIndex(row, 2));
+            string Dgabarit = GetCellValue(document, GetCellAtIndex(row, 3));
+            string d = GetCellValue(document, GetCellAtIndex(row, 4));
+            string L = GetCellValue(document, GetCellAtIndex(row, 5));
+            string Nju = GetCellValue(document, GetCellAtIndex(row, 6));
+            string Hopt = GetCellValue(document, GetCellAtIndex(row, 7));
+            string Freq = GetCellValue(document, GetCellAtIndex(row, 8));
+            string Maker = GetCellValue(document, GetCellAtIndex(row, 9));
+            return new SHVN_Nasos(Name, q100, H, Dgabarit, d, L, Nju, Hopt, Freq, Maker);
+        }
+
+        private static Cell GetCellAtIndex(Row row, int index)
+        {
+            // Проверяем, что индекс находится в пределах допустимого диапазона
+            if (index < row.Elements<Cell>().Count())
+            {
+                return row.Elements<Cell>().ElementAt(index);
+            }
+            else
+            {
+                // Возвращаем фиктивную ячейку или обрабатываем выход за пределы диапазона по необходимости
+                return new Cell();
+            }
+        }
+        private static string GetCellValue(SpreadsheetDocument document, Cell c)
+        {
+            SharedStringTablePart sharedStringPart = document.WorkbookPart.SharedStringTablePart;
+
+            if (c.DataType != null && c.DataType.Value == CellValues.SharedString)
+            {
+                int index = int.Parse(c.InnerText);
+                return sharedStringPart.SharedStringTable.Elements<SharedStringItem>().ElementAt(index).InnerText;
+            }
+            else
+            {
+                return c.InnerText;
+            }
+        }
+    }
+}
